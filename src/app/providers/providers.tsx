37d67db07d70233a11997f77c194d95d06c7ea7b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-18 17:57:48 +0600

    store set up

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,12 +1,15 @@
 'use client'
-
 import { ThemeProvider } from '@mui/material';
 import { theme } from '@/app/assests/theme';
+import { Provider } from 'react-redux';
+import { store } from '@/app/rtk-store/rtk-store';
 
 function Providers( { children }: { children: React.ReactNode }) {
     return (
         <ThemeProvider theme={theme}>
-            {children}
+            <Provider store={store}>
+                {children}
+            </Provider>
         </ThemeProvider>
     )
 }
