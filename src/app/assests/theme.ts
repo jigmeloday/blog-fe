ce172078e75216a07b67f820856e56acf25f7492
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-18 23:51:06 +0600

    theme setting

--- src/app/assests/theme.ts
+++ src/app/assests/theme.ts
@@ -6,6 +6,16 @@ export const theme = createTheme({
    main : '#0079A2',
    light : '#F9EAF3',
    dark : 'rgba(0,86,115,0.50)'
-  }
+  },
+  success: {
+   main: '#76D03F'
+  },
+  error: {
+   main: '#E74C3C'
+  },
+  warning: {
+   main: '#F39C12',
+   contrastText: '#FFFFFF'
+  },
  }
 });
