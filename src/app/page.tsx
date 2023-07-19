842b2bdb574e4f693a1d12a22971961254ab89b3
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-19 13:45:48 +0600

    ui-kits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,19 @@
 import { Grid } from '@mui/material';
 import Input from '@/app/shared/component/input/input.component';
 import Button from '@/app/shared/component/button/button.component';
+import Dropdown from '@/app/shared/component/dropdown/dropdown';
 
 export default function Home () {
+ const val = [
+  'hello',
+  'hi', 'bye'
+ ];
  return (
   <Grid py='120px'>
-   <Input name={'hello'} placeholder='hello' label='Hello' variant='outlined' />
-   <Button label='HELLO' variant='contained' />
-   <Button label='outlined' variant='outlined' />
+   <Input name={ 'hello' } placeholder='hello' label='Hello' variant='outlined'/>
+   <Button label='HELLO' variant='contained'/>
+   <Button label='outlined' variant='outlined'/>
+   <Dropdown value={val} label='Test'/>
   </Grid>
  );
 }
