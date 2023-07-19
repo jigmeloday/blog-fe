4ae3df1b1d05a1ac9faa3c8bea8e5464f3cc9d76
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-19 17:02:32 +0600

    rtk-qiery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 import { Grid } from '@mui/material';
-import { theme } from '@/app/assests/theme';
 
 export default function Home () {
  return (
-  <Grid bgcolor={theme.palette.primary.main}>
-      hello
-  </Grid>
+  <Grid >
+   {process.env.NEXT_PUBLIC_BASE_URL}
+hello  </Grid>
  );
 }
---
+++ src/app/services/api/test.slice.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { FetchBaseQueryArgs } from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
+import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
+
+export const pokemonApi = createApi({
+ reducerPath: 'pokemonApi',
+ baseQuery: fetchBaseQuery({
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL
+ }as FetchBaseQueryArgs),
+ endpoints: (builder: EndpointBuilder<any, any, any>) => ({
+  getPokemonByName: builder.query<any, string>({
+   query: () => '/posts'
+  }),
+  getPokemonByID: builder.query<any, string>({
+   query: () => '/posts/1'
+  }),
+ }),
+});
+
+export const { useGetPokemonByNameQuery } = pokemonApi;
+export const { useGetPokemonByIDQuery } = pokemonApi;
