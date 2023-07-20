c6da13abda9c081874e8048cf8a28ab8480eb4a5
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-20 17:34:39 +0600

    auth form layouting

--- src/app/auth/component/auth-container.tsx
+++ src/app/auth/component/auth-container.tsx
@@ -1,13 +1,18 @@
 'use client';
 import { Grid } from '@mui/material';
 import { ReactNode } from 'react';
-import { FormContainer, LeftSide, RightSide } from '@/app/auth/component/style/auth-style';
+import { FormContainer, Background } from '@/app/auth/component/style/auth-style';
+import { theme } from '../../../../styles/theme';
 
 export default function AuthContainer (props: ReactNode) {
  return(
-  <Grid className='position--relative' item container direction='row' border='1px solid black'>
-   <RightSide xs={8}/>
-   <LeftSide />
+  <Grid className='position--relative' item container direction='row'>
+   <Background xs={8}/>
+   <Background
+    background={ `
+       var(--gradient, linear-gradient(225deg, ${theme.palette.primary.dark} 0%, ${theme.palette.primary.main} 100%))
+       ` }
+    xs={4}/>
    <FormContainer>
     {props.children}
    </FormContainer>
--- src/app/auth/component/style/auth-style.ts
+++ src/app/auth/component/style/auth-style.ts
@@ -1,21 +1,19 @@
 import { styled } from '@mui/system';
 import { Grid } from '@mui/material';
 
-export const RightSide = styled(Grid)(
- ({}) => `
+export const Background = styled(Grid)(
+ ({...props}) => `
     height: 100vh;
-    border: 1px solid black;
-    `
-);
-
-export const LeftSide = styled(Grid)(
- ({}) => `
-    height: 100vh;
-    border: 1px solid black;
-    `
-);
+    background: ${props.background};
+ `);
 
 export const FormContainer = styled(Grid)(
- ({}) => `
+ ({theme}) => `
     position: absolute;
+    left: 60%;
+    top: 50%;
+    padding: 32px;
+    background: ${theme.palette.white.main};
+    border-radius: 8px;
+    box-shadow: 0px 2px 4px 0px rgba(138, 193, 212, 0.24);
  `);
