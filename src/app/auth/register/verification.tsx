8616151a7ec1d0a1780fa7479d866a5aa25075d9
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-25 11:59:30 +0600

    email verification and registetation

--- src/app/auth/register/verification.tsx
+++ src/app/auth/register/verification.tsx
@@ -1,29 +1,25 @@
 'use client';
 import { Grid } from '@mui/material';
 import Typography from '@/shared/component/typography/typography';
-import VerificationInput from 'react-verification-input';
 import Button from '@/shared/component/button/button.component';
 import { useEffect, useState } from 'react';
 import { useConfirmationMutation, useResendConfirmationMutation } from '@/app/auth/services/api-service/api-service';
 import { useRouter } from 'next/navigation';
+import Input from '@/shared/component/input/input.component';
+import { Formik, FormikValues } from 'formik';
 
 export default function Verification (props: { email: string }) {
- const [valid, setValid] = useState<string>( '' );
- const [confirmation] = useConfirmationMutation();
+ const [confirmation, { data }] = useConfirmationMutation();
  const [resendConfirmation] = useResendConfirmationMutation();
  const route = useRouter();
 
  useEffect(() => {
-  confirmation && route.push('/auth/login');
+  data && route.push('/auth/login');
  }, [confirmation]);
 
- const verification = ( event?: string ): void => {
-  if ( event?.length === 6 ) {
-   setValid( event );
-  }
- };
- const submitCode =() => {
-  valid && confirmation(valid);
+
+ const submitCode =(values: FormikValues) => {
+  values && confirmation(values.code);
  };
  const resend = () => {
   resendConfirmation({
@@ -31,32 +27,24 @@ export default function Verification (props: { email: string }) {
   });
  };
  return(
-  <Grid item container py='100px' direction='column' alignItems='center' justifyContent='center'>
-   <Typography label='Verification' variant='body1' fontSize='32px' fontWeight='600' />
-   <Typography label='Enter the OTP' variant='caption' fontSize='16px' />
-   <Grid item container my='32px'>
-    <VerificationInput
-     name='code'
-     onChange={verification}
-     classNames={{
-      container: 'container',
-      character: 'character',
-      characterInactive: 'character--inactive',
-      characterSelected: 'character--selected',
-     }}
-     inputProps={{ autoComplete: 'one-time-code' }}
-     validChars="0-9"
-     placeholder="_"
-    />
-   </Grid>
-   <Grid item container direction='row' justifyContent='center' gap='8px' mb='20px'>
-    <Typography label='Didn’t receive OTP?' variant='caption' />
-    <Typography 
-     click={() => resend()} 
-     label='Resend OTP' 
-     variant='caption' color='primary.main' className='cursor--pointer' />
-   </Grid>
-   <Button click={submitCode} label='Submit' variant='contained' className='width--full' />
-  </Grid>
+  <Formik initialValues={{ code: '' }} onSubmit={(values) => submitCode(values)}>
+   {({handleChange, handleSubmit}) => (
+    <Grid item container py='100px' direction='column' alignItems='center' justifyContent='center'>
+     <Typography label='Verification' variant='body1' fontSize='32px' fontWeight='600' />
+     <Typography label='Enter the OTP' variant='caption' fontSize='16px' />
+     <Grid item container my='32px'>
+      <Input name='code' onChange={handleChange} variant='outlined' />
+     </Grid>
+     <Grid item container direction='row' justifyContent='center' gap='8px' mb='20px'>
+      <Typography label='Didn’t receive OTP?' variant='caption' />
+      <Typography
+       click={() => resend()}
+       label='Resend OTP'
+       variant='caption' color='primary.main' className='cursor--pointer' />
+     </Grid>
+     <Button click={handleSubmit} label='Submit' variant='contained' className='width--full' />
+    </Grid>
+   )}
+  </Formik>
  );
 };
