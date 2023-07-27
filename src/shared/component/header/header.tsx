700666447d285dfd0aaa559ea8af03f237508a3d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-27 13:27:19 +0600

    header

---
+++ src/shared/component/header/header.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { CustomHeader } from '@/shared/component/header/style/header.style';
+
+export default function Header () {
+ return(
+  <CustomHeader>
+hello
+  </CustomHeader>
+ );
+}
---
+++ src/shared/component/header/style/header.style.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/system';
+import { Grid } from '@mui/material';
+
+export const CustomHeader = styled(Grid)(
+ ({}) => `
+   padding: 14px 80px;
+   width: 100%;
+   box-shadow: 0px 2px 4px 0px rgba(138, 193, 212, 0.24);
+ `
+);
