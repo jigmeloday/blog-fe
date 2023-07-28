fd15fcf5ee83935733fd57e14a91e5046db6dcec
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-07-28 17:48:07 +0600

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 'use client';
 import Header from '@/shared/component/header/header';
+import { Box, Grid } from '@mui/material';
+import { SubHeader } from '@/shared/component/header/style/header.style';
+import Button from '@/shared/component/button/button.component';
+import Icon from '@/shared/component/icon/icon';
+import { FILTER_CHIP } from '@/shared/component/header/constant/header.constant';
 
 export default function Home () {
 
  return (
-  <Header/>
+  <Grid container>
+   <Header/>
+   <Grid item container px='80px' my='24px'>
+    <SubHeader item container direction='row' py='8px' alignItems='center'>
+     <Button label='New Post' startIcon={<Icon iconName='add' color='primary.main' />} />
+     <Grid items container ml='80px' xs='auto'>
+      {
+       FILTER_CHIP.map(({ label, value }, index) => (
+        <Box px='24px' ml={index ? '16px': '0'} key={value} className='cursor--pointer'>
+         {label}
+        </Box>
+       ))
+      }
+     </Grid>
+    </SubHeader>
+   </Grid>
+  </Grid>
  );
 }
--- src/shared/component/header/constant/header.constant.ts
+++ src/shared/component/header/constant/header.constant.ts
@@ -1,4 +1,4 @@
-import { HeaderIconModel } from '@/shared/component/header/model/header.model';
+import { FilterChip, HeaderIconModel } from '@/shared/component/header/model/header.model';
 
 export const HEADER_ICON: HeaderIconModel[] = [
  {
@@ -6,5 +6,24 @@ export const HEADER_ICON: HeaderIconModel[] = [
  },
  {
   name: 'notifications_none'
+ }
+];
+
+export const FILTER_CHIP: FilterChip[] = [
+ {
+  label: 'All',
+  value: 'all'
+ },
+ {
+  label: 'For You',
+  value: 'for_you'
+ },
+ {
+  label: 'Popular',
+  value: 'popular'
+ },
+ {
+  label: 'Following',
+  value: 'following'
  }
 ];
