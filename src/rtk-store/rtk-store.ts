4892e5467bfc7dedd4f288a66dc3e3c5268bb8b6
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-14 17:28:25 +0600

    fix: authenticated

--- src/rtk-store/rtk-store.ts
+++ src/rtk-store/rtk-store.ts
@@ -3,17 +3,23 @@ import { articleAPI } from '@/app/services/api/article.api';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { authAPI } from '@/app/services/api/auth.api';
 import { popularAPI } from '@/app/services/api/get-popular.api';
+import { userAPI } from '@/app/services/api/user.api';
 
 
 export const store = configureStore({
  reducer: {
   [articleAPI.reducerPath]: articleAPI.reducer,
   [authAPI.reducerPath]: authAPI.reducer,
-  [popularAPI.reducerPath]: popularAPI.reducer
+  [popularAPI.reducerPath]: popularAPI.reducer,
+  [userAPI.reducerPath]: userAPI.reducer
 
  },
  middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(authAPI.middleware).concat(articleAPI.middleware).concat(popularAPI.middleware),
+  getDefaultMiddleware()
+   .concat(authAPI.middleware)
+   .concat(articleAPI.middleware)
+   .concat(popularAPI.middleware)
+   .concat(userAPI.middleware),
 });
 
 setupListeners(store.dispatch as any);
