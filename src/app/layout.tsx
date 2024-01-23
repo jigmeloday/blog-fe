d3399a0df32ec6bc7a37f4fe2081c43ac5dd8c4d
Jigme Lodey <jigmenlodey@gmail.com>
2024-01-23 14:10:26 +0600

    laylout indentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout ({
    <body className={inter.className}>
     <div className='container'>
      <div className='wrapper'>
-     <Header/>{children}<Footer/>
+      <Header/>{children}<Footer/>
      </div>
     </div>
    </body>
