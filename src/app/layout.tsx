4b24661e720e75efe34761e7bc4475214394665c
Jigme Lodey <jigmenlodey@gmail.com>
2024-01-23 12:57:03 +0600

    project set up `

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Providers from '@/providers/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,9 +21,7 @@ export default function RootLayout ({
      rel="stylesheet"/>
    </head>
    <body className={inter.className}>
-    <Providers>
-     {children}
-    </Providers>
+    {children}
    </body>
   </html>
  );
